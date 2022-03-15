8db8fe860a1b6e54fbae96494dd2788bf1aae579
Mayank Gupta <guptamayankwork@gmail.com>
2022-03-15 13:04:30 +0530

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { UsercrudComponent } from './usercrud/usercrud.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard' , component:DashboardComponent},
+  { path: '',component:LoginPageComponent},
+  { path: 'usercrud',component:UsercrudComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [DashboardComponent,LoginPageComponent,UsercrudComponent]
---
+++ src/app/login-page/login-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
